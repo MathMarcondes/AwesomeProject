6dc3c2da986e90f5e58ac748e512fa6d8610b28d
MathMarcondes <matheus.marcondes@outlook.com>
2024-10-09 20:07:28 -0300

    Estruturado

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+
+import { Text } from "react-native";
+import { Home } from "./screens";
+
+export default function App() {
+    return (
+        <Home title="Home">
+            <Text>
+                Olá, Mundo
+            </Text>
+        </Home>
+    )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+import { Text, View } from "react-native";
+
+interface HomeProps extends PropsWithChildren {
+    title: string;
+}
+
+export function Home({children, title}:HomeProps) {
+    return (
+        <View>
+            {children}
+            <Text>
+                {title}
+            </Text>
+        </View>
+    )
+}
