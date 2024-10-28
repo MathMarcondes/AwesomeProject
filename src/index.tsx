699baefa4448cef8197393d3e9418ce1f74b000f
MathMarcondes <matheus.marcondes@outlook.com>
2024-10-28 14:49:15 -0300

    Parte 1 de estilização.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StatusBar } from 'react-native';
-
+import Home from './screens/Home';
 import Routes from './routes';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -10,7 +10,10 @@ export default function App(){
   return(
     <NavigationContainer>
       <StatusBar backgroundColor="#38A69D" barStyle="light-content"/>
-      <Routes/>
+        <Home/>
+        
+
+
     </NavigationContainer>
   )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,35 +1,77 @@
 import { PropsWithChildren } from "react";
 import { Text, View, Button, Image, StyleSheet, TouchableOpacity } from "react-native";
-
+import React from "react";
 
 export default function Home () {
-    return(
+    return (
         <View style={styles.container}>
-            
-
-            <View>
+            <View style={styles.containerLogo}>
                 <Image
-                source={require('../../Images/logo.png')}
-                style={{width: '100%'}}
-                resizeMode="contain"/>
+                    source={require('../../Images/logo.png')}
+                    style={{ width: '100%' }}
+                    resizeMode="contain"
+                />
             </View>
 
-
-            <View style={styles.container}>
-                <Text>Monitore, organize seus gastos de qualquer lugar!</Text>
+            <View style={styles.containerForm}>
+                <Text style={styles.title}>Monitore, organize seus gastos de qualquer lugar!</Text>
                 <Text style={styles.text}>Faça o login para começar</Text>
+                
+               
                 <TouchableOpacity style={styles.button}>
-                    <Text>Acessar</Text>
+                    <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
+      
             </View>
-
         </View>
     );
 }
 
-
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#38A69D',
+    },
+    containerLogo: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12,
+        color: 'black',
+    },
 
+    text: {
+        color: '#a1a1a1',
+    },
+    button:{
+        position: 'absolute',
+        backgroundColor: '#38a69d',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '15%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold'
     }
-})
+
+});
