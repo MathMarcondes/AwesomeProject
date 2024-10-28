59652fc1bd8d05d118a7887d1fbfa895154e6459
MathMarcondes <matheus.marcondes@outlook.com>
2024-10-27 23:17:03 -0300

    new project - rebuild

--- src/index.tsx
+++ src/index.tsx
@@ -3,28 +3,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import TelaLogin from './screens/log';
-import Copo from './screens/Copo';
-import Email from './screens/Email';
+
 
 
 const Stack = createNativeStackNavigator();
 
-
-const Screen = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={TelaLogin}
-        options={{headerShown: false}} />
-        <Stack.Screen name="Copo" component={Copo}
-        options={{headerShown: false}} />
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-  };    
-export default Screen;
+export default function screens() {
+  
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,41 +2,15 @@ import { PropsWithChildren } from "react";
 import { Text, View, StyleSheet, Button, Image, ImageBackground } from "react-native";
 
 
-export default function Home ({navigation}) {
+export default function Home () {
     return(
         <View style={styles.container}>
             
-            <View>
-            <Image source={require('../../Images/logo-insta.png')} style={styles.image}/>
-            </View>
-            <Text style={styles.text}>Home Screen</Text>
-
-            <Button 
-            title="Go to Login"
-            onPress={() => navigation.navigate("Login")}
-
-            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-    fontSize: 30,
-    backgroundColor: "#4d0092",
-    },
-    Button: {
-    position: 'absolute',
-    borderRadius: 50,
-    paddingVertical: 8,
-    width: '60%',
-    alignSelf: 'center',
-    bottom: '15%'
-    }
+    
     
 })
