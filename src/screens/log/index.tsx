9ec936f95721391853fe6cd1e75427315ea229e7
MathMarcondes <matheus.marcondes@outlook.com>
2024-10-30 12:37:22 -0300

    Tela de Login realizada.

--- src/screens/log/index.tsx
+++ src/screens/log/index.tsx
@@ -3,96 +3,93 @@ import React, { useState } from 'react';
 import { StyleSheet, TextInput, View, Text, TouchableOpacity} from 'react-native';
 import * as Animatable from 'react-native-animatable'
 
-
-export default function Login  () {
-
-    return (
+export default function Login() {
+    return(
       <View style={styles.container}>
-        <Animatable.View animation="fadeInIt" delay={500} style={styles.containerHeader}>
-          <Text style={styles.message}>Bem vindo(a)</Text>
+        <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+          <Text style={styles.message}>Bem Vindo!</Text>
         </Animatable.View>
-      
-      
-        <Animatable.View animation='fadeInUp' style={styles.containerForm}>
+
+        <Animatable.View animation="fadeInUp" delay={500} style={styles.containerForm}>
           <Text style={styles.title}>Email</Text>
-          <TextInput
-              placeholder="Digite seu email"
-              style={styles.input}
-          />
+          <TextInput placeholder='Seu email' style={styles.input}/>
+
           <Text style={styles.title}>Senha</Text>
-          <TextInput
-              placeholder="Digite sua senha"
-              style={styles.input}
-          />
+          <TextInput placeholder='Sua senha' style={styles.input}/>
+        
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Acessar</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>Não possui uma conta? Cadastre-se!</Text>
+        </TouchableOpacity>
 
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
-          </TouchableOpacity>
 
         </Animatable.View>
-      
-      
       </View>
     )
 }
- 
+
+
 const styles = StyleSheet.create({
-      container:{
-        flex: 2,
-        backgroundColor: '#38a69d',
-      },
-      containerHeader:{
-        marginTop: '14%',
-        marginBottom: '8%',
-        paddingStart: '5%',
-      },
-      message:{
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#FFF'
-      },
-      containerForm:{
-        backgroundColor: '#FFF',
-        flex: 1,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25, 
-      },
-      title:{
-        fontSize: 20,
-        margin: 28,
-      },
-      input:{
-        borderBottomWidth: 1,
-        height: 40,
-        marginBottom: 12,
-        fontSize: 16,
-      },
-      button:{
-        backgroundColor: '#38a69d',
-        width: '100%',
-        borderRadius: 4,
-        paddingVertical: 8,
-        marginTop: 14,
-        justifyContent: 'center',
-        alignContent: 'center'
-      },
-      buttonText:{
-        color: '#FFF',
-        fontSize: 11,
-        fontWeight: 'bold'
-      },
-      buttonRegister: {
-        marginTop: 14,
-        alignSelf: 'center'
-      },
-      registerText: {
-        color: '#a1a1a1'
-      }
-      
-    
+  container:{
+    flex: 1,
+    backgroundColor: '#38a69d'
+  },
+  containerHeader:{
+    marginTop:'14%',
+    marginBottom: '8%',
+    paddingStart: '5%'
+  },
+  message:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+ },
+ containerForm:{
+  backgroundColor: '#fff',
+  flex: 1,
+  borderTopLeftRadius: 25,
+  borderTopRightRadius: 25,
+  paddingStart: '5%',
+  paddingEnd: '5%',
+ },
+ title:{
+  fontSize: 25,
+  marginTop: 28,
+  color: 'black',
+  fontWeight: 'bold'
+ },
+
+ input:{
+  borderBottomWidth: 1,
+  height: 40,
+  marginBottom: 12,
+  fontSize: 16,
+  color: '#a1a1a1'
+ },
+ button:{
+  backgroundColor: '#38a69d',
+  width: '100%',
+  borderRadius: 4,
+  paddingVertical: 8,
+  marginTop: 14,
+  justifyContent: 'center',
+  alignItems: 'center'
+ },
+buttonText:{
+  color:'#fff',
+  fontSize: 18,
+  fontWeight: 'bold',  
+},
+buttonRegister: {
+  marginTop: 14,
+  alignSelf: 'center',
+},
+registerText:{
+  color: '#a1a1a1'
+}
+
+
 })
